0b7f8ac0203c831a3ba828732de638a8dabb7c7e
AbdullahDanismaz <98493686+AbdullahDanismaz@users.noreply.github.com>
2022-05-07 20:29:53 +0100

    day2 T1_CydeoVerifications first commit

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class T1_CydeoVerifications {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+TEST cASE #1:Cydeo practice tool verification
+1. Open Chrome browser
+//2.Go to https:practice.cydeo.com
+3.Verify URL contains
+Expected:cydeo
+4.Verify Title:
+Expected:Practise
+
+ */
